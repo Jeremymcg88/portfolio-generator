95cc83592ff0fc4e7f928f9dfc38dcd6261a061f
Jeremymcg88 <jeremy.mcguirk@gmail.com>
2021-08-14 01:04:59 -0400

    added validation property to question array

--- app.js
+++ app.js
@@ -22,7 +22,15 @@ const promptUser = () => {
         {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username'
+        message: 'Enter your GitHub Username (Required)',
+        validate: gitNameInput => {
+            if (gitNameInput) {
+              return true;
+            } else {
+              console.log('Please enter your GitHub Username!');
+              return false;
+            }
+          }
       },
       {
         type: 'input',
@@ -49,12 +57,28 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project? (Required)',
+        validate: projectNameInput => {
+            if (projectNameInput) {
+              return true;
+            } else {
+              console.log('Please enter the name of your project!');
+              return false;
+            }
+          }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: projectDiscInput => {
+            if (projectDiscInput) {
+              return true;
+            } else {
+              console.log('Please enter a description of your project!');
+              return false;
+            }
+          }
       },
       {
         type: 'checkbox',
@@ -65,7 +89,15 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: linkInput => {
+            if (linkInput) {
+              return true;
+            } else {
+              console.log('Please enter your GitHub link!');
+              return false;
+            }
+          }
       },
       {
         type: 'confirm',
